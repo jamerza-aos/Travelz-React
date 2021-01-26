97a2c27ecae701bd3604fbaec0bdbcd119547d0b
aos-jamerza <awsjamerza@gmail.com>
2021-01-26 22:19:32 +0100

    partner

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -1,9 +1,70 @@
 
 const Partner = () => {
     return (  
-        <main>
-            
-            <h1>Partner</h1>
+        <main className="partnerMain">
+           <section className="partnerSec">
+               <h1>Testimonials</h1>
+               <article className="partArticle1">
+                   <div>
+                       <ul>
+                           <li className="starts">
+                           <img  src="../assets/img/staaar.png" alt=""/>
+                           <img  src="../assets/img/staaar.png" alt=""/>
+                           <img  src="../assets/img/staaar.png" alt=""/>
+                           <img  src="../assets/img/staaar.png" alt=""/>
+                           <img  src="../assets/img/staaar.png" alt=""/>
+                           </li>
+                           <li className="“Quisque">
+                           “Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”
+                           </li>
+                           <li className="Edward">Edward Newgate</li>
+                           <li className="Founder">Founder Circle</li>
+                       </ul>
+                   </div>
+                   <div>
+                       <div className="right">
+                        <img className="previous"  src="../assets/img/previous.png" alt=""/>
+                        <img className="next"  src="../assets/img/next.png" alt=""/>
+                        </div>
+                   </div>
+               </article>
+
+           </section>
+
+            <section className="SECT">
+            <h1>Trending stories <span>View all</span></h1>
+                <article className="trending">
+                <div>
+                    <img   src="../assets/img/image12.png" alt=""/>
+                    <h1>The many benefits of  </h1>
+                    <h1>taking a healing holiday</h1>
+                    <p>‘Helaing holidays’ are on the risetohelp maximise your health and happines...</p>
+                    <a href="">Read more</a>
+               </div>
+
+               <div>
+                    <img   src="../assets/img/image13.png" alt=""/>
+                    <h1>The best Kyoto restaurant </h1>
+                    <h1>to try Japanese food</h1>
+                    <p>From tofu to teahouses, here’s our guide to Kyoto’s best restaurantsto visit...</p>
+                    <a href="">Read more</a>
+               </div>
+               <div>
+                    <img   src="../assets/img/image14.png" alt=""/>
+                    <h1>Skip Chichen Itza and head </h1>
+                    <h1>to this remote Yucatan</h1>
+                    <p>It’s remote and challenging to get,but braving the jungle and exploringthese ruins without the...</p>
+                       <a href="">Read more</a>
+               </div>
+               <div>
+                    <img   src="../assets/img/image15.png" alt=""/>
+                    <h1>Surf’s up at these beginner</h1>
+                    <h1>spots around the world</h1>
+                    <p>If learning to surf has in on your to-do list for a while, the good newsis: it’s never too late...</p>
+                    <a href="">Read more</a>
+               </div>
+           </article>
+           </section>
         </main>
         
     );
