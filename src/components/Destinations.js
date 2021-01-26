4faacc679880c37c087bf42e8100b90b4b6f76d0
aos-jamerza <awsjamerza@gmail.com>
2021-01-26 21:13:05 +0100

    dietinations

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,7 +1,44 @@
 
 const Destinations = () => {
     return (  
-        <h1>Destinations</h1>
+        <main className="DesMain">
+            <section className="DesSection">
+            <img id="scoarDes" src="../assets/img/scoar.png" alt=""/>
+            <img id="plusDes"  src="../assets/img/plus.png" alt=""/>
+
+                <article className="Featured">
+                    <h1>Featured destinations</h1>
+                    <h4>View all </h4>
+                </article>
+                <article className="photo">
+                    <div className="Raja">
+                        <div className="text">
+                            <h3>Raja Ampat</h3>
+                            <h5>Indonesia</h5>
+                        </div>
+                    </div>
+                    <div className="Fan">
+                        <div className="text">
+                            <h3>Fanjingshan</h3>
+                         <h5>China</h5>
+                        </div>
+                    </div>
+                    <div className="Vevey">
+                        <div className="text">
+                            <h3>Vevey </h3>
+                            <h5>Switzerland</h5>
+                        </div>
+                    </div>
+                    <div className="Skadar">
+                        <div className="text">
+                        <   h3>Skadar Montenegro</h3>
+                            <h5>Montenegro</h5>
+                        </div>
+                    </div>
+                </article>
+            </section>
+            <img id="scoarDes2"  src="../assets/img/scoar.png" alt=""/>
+        </main>
     );
 }
  
