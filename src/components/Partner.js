a4a1b9ac8c5b0a923f7125fcefb17e41e972b7b2
aos-jamerza <awsjamerza@gmail.com>
2021-01-27 15:32:42 +0100

    done

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -16,7 +16,7 @@ const Partner = () => {
                            <img  src="../assets/img/staaar.png" alt=""/>
                            <img  src="../assets/img/staaar.png" alt=""/>
                            </li>
-                           <li className="“Quisque">
+                           <li className="Quisque">
                            “Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”
                            </li>
                            <li className="Edward">Edward Newgate</li>
@@ -42,30 +42,30 @@ const Partner = () => {
                 <article className="trending">
                 <div>
                     <img   src="../assets/img/image12.png" alt=""/>
-                    <h1>The many benefits of  </h1>
-                    <h1>taking a healing holiday</h1>
+                    <h2>The many benefits of  </h2>
+                    <h2>taking a healing holiday</h2>
                     <p>‘Helaing holidays’ are on the risetohelp maximise your health and happines...</p>
                     <a href="">Read more</a>
                </div>
 
                <div>
                     <img   src="../assets/img/image13.png" alt=""/>
-                    <h1>The best Kyoto restaurant </h1>
-                    <h1>to try Japanese food</h1>
+                    <h2>The best Kyoto restaurant </h2>
+                    <h2>to try Japanese food</h2>
                     <p>From tofu to teahouses, here’s our guide to Kyoto’s best restaurantsto visit...</p>
                     <a href="">Read more</a>
                </div>
                <div>
                     <img   src="../assets/img/image14.png" alt=""/>
-                    <h1>Skip Chichen Itza and head </h1>
-                    <h1>to this remote Yucatan</h1>
+                    <h2>Skip Chichen Itza and head </h2>
+                    <h2>to this remote Yucatan</h2>
                     <p>It’s remote and challenging to get,but braving the jungle and exploringthese ruins without the...</p>
                        <a href="">Read more</a>
                </div>
                <div>
                     <img   src="../assets/img/image15.png" alt=""/>
-                    <h1>Surf’s up at these beginner</h1>
-                    <h1>spots around the world</h1>
+                    <h2>Surf’s up at these beginner</h2>
+                    <h2>spots around the world</h2>
                     <p>If learning to surf has in on your to-do list for a while, the good newsis: it’s never too late...</p>
                     <a href="">Read more</a>
                </div>
